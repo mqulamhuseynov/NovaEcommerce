ee4f51be3c150bbe64766454058c855dd2c47dcd
mqulamhuseynov <mqulamhuseynov@gmail.com>
2026-07-06 12:13:42 +0400

    _initial

---
+++ NovaEcommerceAPI/src/NovaEcommerce.Domain/Entities/Commons/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaEcommerce.Domain.Entities.Commons
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
