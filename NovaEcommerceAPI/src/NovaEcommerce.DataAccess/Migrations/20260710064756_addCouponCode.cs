a6d5e2b6fdeca522b41aea57101936dfd1752476
mqulamhuseynov <mqulamhuseynov@gmail.com>
2026-07-10 12:28:24 +0400

    gitignore problemi

---
+++ NovaEcommerceAPI/src/NovaEcommerce.DataAccess/Migrations/20260710064756_addCouponCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NovaEcommerce.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCouponCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AppliedCouponCode",
+                table: "Carts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppliedCouponCode",
+                table: "Carts");
+        }
+    }
+}
