6d5856fd7d543795c96234c3a0e614e92e112a3d
mqulamhuseynov <mqulamhuseynov@gmail.com>
2026-07-19 21:45:03 +0400

    appdbcontext problem

--- NovaEcommerceAPI/src/NovaEcommerce.DataAccess/DbContext/AppDbContext.cs
+++ NovaEcommerceAPI/src/NovaEcommerce.DataAccess/DbContext/AppDbContext.cs
@@ -35,6 +35,9 @@ public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<int>, int>
     public DbSet<Faq> Faqs => Set<Faq>();
     public DbSet<SupportArticle> SupportArticles => Set<SupportArticle>();
     public DbSet<Notification> Notifications => Set<Notification>();
+    public DbSet<HeroBanner> HeroBanners => Set<HeroBanner>();
+    public DbSet<NotifyRequest> NotifyRequests => Set<NotifyRequest>();
+    public DbSet<CheckoutSession> CheckoutSessions => Set<CheckoutSession>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
