767d118ea45717a407efb5858c66ee8757a7cfce
Qulamhüseyn Muhamməd <mqulamhuseynov@gmail.com>
2026-07-23 10:50:20 +0400

    Revert "Feature/user profile"

--- NovaEcommerceAPI/src/NovaEcommerce.ServicesApp/Services/Interfaces/Service/IFileStorageService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace NovaEcommerce.ServicesApp.Services.Interfaces.Service;
-
-public interface IFileStorageService
-{
-    Task<string> SaveAvatarAsync(IFormFile file);
-}
