d673d9166a91ac971442f5fd8fe282d3849f6523
Nail2005 <githubnail2005@gmail.com>
2026-07-25 15:51:16 +0400

    Auth Validation added

---
+++ NovaEcommerceAPI/src/NovaEcommerce.ServicesApp/FluentValidations/LoginValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using NovaEcommerce.ServicesApp.DTOs.AuthDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaEcommerce.ServicesApp.FluentValidations
+{
+    public class LoginValidator : AbstractValidator<LoginRequestDto>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+            
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+        }
+    }
+}
---
+++ NovaEcommerceAPI/src/NovaEcommerce.ServicesApp/FluentValidations/RegisterValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using NovaEcommerce.ServicesApp.DTOs.AuthDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaEcommerce.ServicesApp.FluentValidation
+{
+    public class RegisterValidator : AbstractValidator<RegisterRequestDto> 
+    {
+        public RegisterValidator() 
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(50)
+                .Matches(@"^[A-Za-zƏəIıİiÖöĞğÜüÇçŞş]+$")
+                .WithMessage("First name can only contain letters.");
+            
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(50)
+                .Matches(@"^[A-Za-zƏəIıİiÖöĞğÜüÇçŞş]+$")
+                .WithMessage("Last name can only contain letters.");
+            
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress(); 
+            
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+        } 
+    }
+}
